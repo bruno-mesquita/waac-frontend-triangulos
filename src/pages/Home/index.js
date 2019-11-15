765a988141c173a808f614f888d2815e74ef354b
Bruno Silva <bmesquita196@gmail.com>
2019-11-15 11:15:10 -0300

    criacao pagina home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
+import { Button } from '@material-ui/core';
+
+import { Container, Paper, Entrada, Saida, Input } from './styles';
+
 export default function Home() {
   return (
-    <>
+    <Container>
       <h1>Waac</h1>
-    </>
+      <Paper>
+        <Entrada>
+          <h2>Desafio</h2>
+          <p>
+            Descrição: Dado um triângulo de números, encontre o total máximo de
+            cima para baixo.
+          </p>
+          <p>
+            Exemplo: [[6],[3,5],[9,7,1],[4,6,8,4]], cada prosição da matriz deve
+            ser inserida em uma linha.
+          </p>
+
+          <Input label="Linha 1" placeholder="Exemplo: 6" />
+          <Button variant="contained" color="primary">
+            Calcular
+          </Button>
+        </Entrada>
+        <Saida>
+          <h1>Resultado</h1>
+        </Saida>
+      </Paper>
+    </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,3 +1,54 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+import { TextField } from '@material-ui/core';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    font-size: 50px;
+    margin: 50px;
+  }
+`;
+
+export const Paper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+
+  background: white;
+  padding: 15px;
+  border-radius: 4px;
+  border: solid 1px;
+  border-color: #d3d3d3;
+`;
+
+export const Entrada = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+
+  h2 {
+    font-size: 30px;
+    align-self: center;
+    margin-bottom: 20px;
+  }
+
+  p {
+    margin-bottom: 15px;
+  }
+`;
+
+export const Saida = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Input = styled(TextField)`
+  width: 60%;
+`;
